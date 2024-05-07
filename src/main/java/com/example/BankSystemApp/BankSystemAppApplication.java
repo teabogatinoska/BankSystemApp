8db12dc2d5e272d91382cf06804613218180977f
TBogatinoska <tea.bogatinoska@endava.com>
2024-05-07 16:54:52 +0200

    Initial commit

---
+++ src/main/java/com/example/BankSystemApp/BankSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankSystemApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BankSystemApp/BankSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankSystemApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
